2783288f518b3f672bc7d1d060065f6897a30152
DC <easetreychen@gmail.com>
2021-09-07 19:45:15 +0800

    create ButtonDemo component

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,15 @@
 <template>
   ButtonDemo
+  <Button>
+    <slot></slot>
+  </Button>
 </template>
 
 <script>
+import Button from "../lib/Button.vue";
 export default {
-  name: "ButtonDemo"
+  name: "ButtonDemo",
+  components:{Button}
 }
 </script>
 
---
+++ src/lib/Button.vue
@@ -0,0 +1,13 @@
+<template>
+<button><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "Button"
+}
+</script>
+
+<style scoped>
+
+</style>
