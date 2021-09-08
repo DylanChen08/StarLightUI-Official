cc4cdba3ac065a1cf0a864c510d5e1b068b621c4
DC <easetreychen@gmail.com>
2021-09-08 17:04:19 +0800

    button 支持 theme/level/disable/loading 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,14 +1,50 @@
 <template>
-
   <button class="starlight-button" :class="{[`starlight-theme-${theme}`]: theme}">
+    <span v-if="loading" class="starlight-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import { computed } from "vue";
 export default {
-  name: "Button"
-}
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    // console.log('setup in button.vue')
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      // console.log(props.level)
+      return {
+        [`starlight-theme-${theme}`]: theme,
+        [`starlight-size-${size}`]: size,
+        [`starlight-level-${level}`]: level,
+      };
+    });
+    // console.log(333);
+    return { classes };
+  },
+};
 </script>
 
 <style lang="scss">
@@ -17,6 +53,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .starlight-button {
   box-sizing: border-box;
   height: $h;
@@ -31,6 +69,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -48,6 +87,136 @@ $radius: 4px;
 
   &::-moz-focus-inner {
     border: 0;
+  }
+
+  &.starlight-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.starlight-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.starlight-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.starlight-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.starlight-theme-button {
+    &.starlight-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.starlight-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.starlight-theme-link {
+    &.starlight-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.starlight-theme-text {
+    &.starlight-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.starlight-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.starlight-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+
+  &.starlight-theme-link, &.starlight-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+
+  > .starlight-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: starlight-spin 1s infinite linear;
+  }
+}
+
+@keyframes starlight-spin {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
   }
 }
 </style>
